4406d8aece4b36d8c80826a3dc846daf1c0abba6
Shreesh Nayak <shreeshnayak@gmail.com>
2020-10-25 21:01:37 -0700

    Added comments for task0b

--- main/final_code/task0b.py
+++ main/final_code/task0b.py
@@ -7,11 +7,12 @@ import sys
 
 tf_idf_dict = {}
 
-
+# returns list of .wrds files
 def get_files_list(dir):
     list_of_files = os.listdir(dir)
     return [ file for file in list_of_files if file.endswith('.wrds')]
 
+# returns count of words across all dimenions
 def count_of_words_across_dimensions(dict_of_words):
     count_of_words = 0
     for each_dimension in dict_of_words:
@@ -29,11 +30,10 @@ def read_file_data(dir, list_of_files):
         word_count_across_dimensions = count_of_words_across_dimensions(dict_of_words)
         tf_idf_dict[str(each_file.split('.')[0])] = dict_of_words
         get_tf_vector(dict_of_words, word_count_across_dimensions, each_file.split('.')[0], dir)
-        # get_tf_idf_vector()
-    # print("prinitng the length of tf_idf_dict ", len(tf_idf_dict))
-
+    
     get_tf_idf_vector(dir)
 
+# returns the tf vector for a particular file
 def get_tf_vector(dict_of_words_across_files, word_count_across_dimensions, file_name, directory):
     the_vectors = {}
     for each_dimension in dict_of_words_across_files:
@@ -41,7 +41,6 @@ def get_tf_vector(dict_of_words_across_files, word_count_across_dimensions, file
             list_of_words = []
             for each_word in dict_of_words_across_files[each_dimension][each_sensor]["words"]:
                 list_of_words.append(tuple(each_word[0]))
-            #print("printing the list of words ", list_of_words)
             set_of_unique_words = list(Counter(tuple(list_of_words)).keys())
             count_of_unique_words = Counter(tuple(list_of_words)).values()
             tf_proportions = [x / (word_count_across_dimensions) for x in count_of_unique_words]
@@ -58,7 +57,7 @@ def get_tf_vector(dict_of_words_across_files, word_count_across_dimensions, file
     file_handler = open(directory + "/" + complete_file_name, 'wb')
     pickle.dump(the_vectors, file_handler)
 
-
+# returns the occurrence of a word across all sensors for a given dimension
 def get_occurence_across_sensors(word, sensor, dimension):
     frequency_of_occurence = 0
     for each_file in tf_idf_dict:
@@ -69,6 +68,7 @@ def get_occurence_across_sensors(word, sensor, dimension):
             frequency_of_occurence = frequency_of_occurence + 1
     return frequency_of_occurence
 
+# calculates and stores the tfidf vector for each file
 def get_tf_idf_vector(dir):
     N = len(tf_idf_dict)
     for each_file in tf_idf_dict:
@@ -83,10 +83,7 @@ def get_tf_idf_vector(dir):
                 for each_word in tf_vector[each_dimension][each_sensor]:
                     m = get_occurence_across_sensors(each_word[0], each_sensor, each_dimension)
                     idf = math.log(N/m)
-                    # print("prininting the word ", each_word)
                     tf = [every_word[1] for every_word in tf_vector[each_dimension][each_sensor] if every_word[0] == tuple(each_word[0])]
-                    # print("prinintg tf vector ", tf_vector[each_dimension][each_sensor])
-                    # print("printing the tf value ", tf)
                     tf_idf_vector[each_dimension][each_sensor].append((tuple(each_word[0]), tf[0] * idf))
 
         complete_file_name = "tfidf_vectors_" + str(each_file) + ".txt"
@@ -103,7 +100,5 @@ if __name__ == '__main__':
     # returns a list of files present in the directory
     list_of_files = get_files_list(directory)
 
+    # reads data from .wrds files and creates tf and tfidf vectors
     read_file_data(directory, list_of_files)
-
-
-
