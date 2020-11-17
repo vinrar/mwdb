8306e88950c5977db8f015a281ac498dc7a8b082
richanagda96 <rjnagda@asu.edu>
2020-11-16 23:12:29 -0700

    Added Phase 3 directory and Task 3 code

---
+++ Phase 3/phase3_task3.py
@@ -0,0 +1,111 @@
+import json
+from collections import defaultdict
+from random import gauss
+
+import numpy as np
+from numpy.linalg import norm
+
+hash_tables, vectors = [], {}
+
+
+def generate_random_unit_vector(dims):
+    vec = [gauss(0, 1) for i in range(dims)]
+    mag = sum(x ** 2 for x in vec) ** .5
+    return [x / mag for x in vec]
+
+
+def get_code_from_vectors(u, v):
+    w = np.dot(np.array(u), np.array(v))
+    return "1" if w >= 0 else "0"
+
+
+def cosine_similarity(u, v):
+    return np.dot(u, v) / (norm(u) * norm(v))
+
+
+def preprocessing(L, k, d):
+    for table in range(L):
+        hashtable = defaultdict(list)
+        gesture_codes = defaultdict(str)
+        for j in range(k):
+            unit_v = generate_random_unit_vector(d)
+            hashtable["unit_vectors"].append(unit_v)
+
+            for gesture, vector in vectors.items():
+                gesture_codes[gesture] += get_code_from_vectors(unit_v, vector)
+
+        for gesture, code in gesture_codes.items():
+            hashtable[code].append(gesture)
+
+        hash_tables.append(hashtable)
+    print_hash_tables()
+
+
+def print_hash_tables():
+    global hash_tables
+    for i, table in enumerate(hash_tables):
+        print("\n---------- Hashtable: ", i + 1, "----------")
+        for key, val in table.items():
+            if key != "unit_vectors":
+                print("Code: ", key, ", Bucket: ", val)
+
+
+def query_algorithm(q):
+    global vectors, hash_tables
+    gestures_to_compare, q_vec, num_buckets = set(), vectors[q], 0
+    for hashtable in hash_tables:
+        unit_vectors, code = hashtable["unit_vectors"], ""
+        for vec in unit_vectors:
+            code += get_code_from_vectors(vec, q_vec)
+        num_buckets += 1
+        for k in hashtable[code]:
+            gestures_to_compare.add(k)
+
+    print("\nNumber of buckets searched: ", num_buckets)
+    print("Total number of gestures in dataset: ", len(vectors.keys()))
+    print("Number of unique gestures compared: ", len(gestures_to_compare))
+    print("Query gesture: ", q)
+    print("List of gestures compared: ", gestures_to_compare)
+
+    distance_map = {}
+    for gesture in gestures_to_compare:
+        distance_map[gesture] = cosine_similarity(vectors[q], vectors[gesture])
+
+    sorted_x = {k: v for k, v in sorted(distance_map.items(), key=lambda item: item[1], reverse=True)}
+    return sorted_x
+
+
+def locality_sensitive_hashing(L, k, query, t):
+    dims = len(list(vectors.values())[0])
+    preprocessing(L, k, dims)
+    result = query_algorithm(query)
+    print("\n-----", t, "most similar gestures using Locality Sensitive Hashing Index structure -----\n")
+    for i, (k, v) in zip(range(t), result.items()):
+        print("Gesture: ", k, ",\tSimilarity Score: ", v)
+
+
+def main():
+    global vectors
+    # L = int(input("Enter the number of layers, L: "))
+    # k = int(input("Enter the number of hashes per layer, k: "))
+    # v_dir = input("Enter directory of vectors: ")
+    # model = input("Enter vector model - tf or tfidf: ")
+    # query_gesture = input("Enter query gesture name, q: ")
+    # t = int(input("Enter number of similar gestures to be returned, t: "))
+
+    L, k, v_dir, model = 10, 6, "3_class_gesture_data", "tf"  # default values
+    query_gesture, t = '10', 10
+    # query_gesture, t = '260', 10
+
+    with open(model + '_vectors.json', 'r') as fp:
+        vectors = json.load(fp)
+
+    locality_sensitive_hashing(L, k, query_gesture, t)
+
+
+if __name__ == '__main__':
+    main()
+
+# Good results for:
+# L, k, v_dir, model = 8, 6, "3_class_gesture_data", "tf"  # default values
+# query_gesture, t = '260' and '588', 10
