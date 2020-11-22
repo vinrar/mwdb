31f59034a8f4f622b3c27f499519b70a8bece93f
Lokesh <lsharma6@asu.edu>
2020-11-22 01:41:08 -0700

    adds task5 integrated with task6

--- Phase 3/phase3_task6.py
+++ Phase 3/phase3_task6.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import tkinter.filedialog as tkFileDialog
 import tkinter.simpledialog as tkSimpleDialog
 import os, json
-from phase3_task3 import locality_sensitive_hashing, preprocessing
+from gui_phase3_task3 import locality_sensitive_hashing, preprocessing
+from task5 import get_ppr2
 
 HEIGHT = 600
 WIDTH = 800
@@ -11,6 +12,8 @@ TASK_NUMBER = None
 QUERY_GESTURE = None
 RELEVANT_FEEDBACK = []
 IRRELEVANT_FEEDBACK = []
+
+
 # L = 10
 # k = 6
 # model = 'tf'
@@ -19,28 +22,32 @@ IRRELEVANT_FEEDBACK = []
 
 # Function to get the directory of gesture files
 def getPath():
-	global GESTURE_PATH
-	filename = tkFileDialog.askdirectory()
-	print('File directory: ',filename)
-	GESTURE_PATH = filename
+    global GESTURE_PATH
+    filename = tkFileDialog.askdirectory()
+    print('File directory: ', filename)
+    GESTURE_PATH = filename
+
 
 # Function to get the task number
 def getNum():
-	global TASK_NUMBER
-	n = tkSimpleDialog.askinteger("Task Number","Enter the task number (4/5)",parent=root,minvalue=4,maxvalue=5)
-	print('Task Number: ',n)
-	TASK_NUMBER = n
+    global TASK_NUMBER
+    n = tkSimpleDialog.askinteger("Task Number", "Enter the task number (4/5)", parent=root, minvalue=4, maxvalue=5)
+    print('Task Number: ', n)
+    TASK_NUMBER = n
+
 
 # Function to get the query gesture
 def getQuery():
     global QUERY_GESTURE
     q = tkSimpleDialog.askinteger("Query Gesture", "Enter the query gesture", parent=root)
-    print('Query Gesture: ',q)
+    print('Query Gesture: ', q)
     QUERY_GESTURE = str(q)
+
 
 # Function to get input for Task 3
 def getTask3Input():
     pass
+
 
 # Function to generate similarity results
 def generateResults():
@@ -49,14 +56,14 @@ def generateResults():
         print("Running task 4")
     if TASK_NUMBER == 5:
         print("Running task 5")
-	
+
     firstRun = True
     runAgain = True
-    
+    gesture_list = []
     while runAgain:
-		# generate results for similarity
+        # generate results for similarity
         if firstRun:
-			# run for the first time without feedback
+            # run for the first time without feedback
 
             # input for LSH
             L = 10
@@ -66,37 +73,47 @@ def generateResults():
             t = 10
             with open(model + '_vectors.json', 'r') as fp:
                 vectors = json.load(fp)
-            
+
             # preprocessing for LSH
             dims = len(list(vectors.values())[0])
             hash_tables = preprocessing(L, k, dims, vectors)
             # locality_sensitive_hashing(L, k, )
-            result = locality_sensitive_hashing(L, k, query_gesture, t, vectors, hash_tables, True)
-            
-            # result = [i for i in range(1,11)]
-            # resultLabel['text'] = formatResult(result)
-            resultLabel['text'] = result
+            results_to_display, gesture_list = locality_sensitive_hashing(L, k, query_gesture, t, vectors, hash_tables,
+                                                                          True)
+
+            # results_to_display = [i for i in range(1,11)]
+            # resultLabel['text'] = formatResult(results_to_display)
+            resultLabel['text'] = results_to_display
             firstRun = False
+            RELEVANT_FEEDBACK.append(query_gesture)
         else:
-			# run for the subsequent times with feedback
-            resultLabel['text'] = formatResult(RELEVANT_FEEDBACK)
-		
-		# get feedback
-        string_relevant_feedback = tkSimpleDialog.askstring("Relevant  Feedback", "Enter the relevant points separated by a comma")
-        string_irrelevant_feedback = tkSimpleDialog.askstring("Irrelevant  Feedback", "Enter the irrelevant points separated by a comma")
-        if string_relevant_feedback is not None and string_relevant_feedback != '':
-            relevant_feedback = [int(f) for f in string_relevant_feedback.split(',')]
-            if string_irrelevant_feedback is not None and string_irrelevant_feedback != '':
-                irrelevant_feedback = [int(f) for f in string_irrelevant_feedback.split(',')]
+            # run for the subsequent times with feedback
+            ppr_results_to_display = get_ppr2(len(gesture_list), 0.85, gesture_list, RELEVANT_FEEDBACK,
+                                              IRRELEVANT_FEEDBACK, gui=True)
+            resultLabel['text'] = ppr_results_to_display
+
+        # get feedback
+        string_relevant_feedback = tkSimpleDialog.askstring("Relevant  Feedback",
+                                                            "Enter the relevant points separated by a comma")
+        string_irrelevant_feedback = tkSimpleDialog.askstring("Irrelevant  Feedback",
+                                                              "Enter the irrelevant points separated by a comma")
+        if string_relevant_feedback and string_relevant_feedback != '':
+            relevant_feedback = string_relevant_feedback.split(',')
+            if string_irrelevant_feedback and string_irrelevant_feedback != '':
+                irrelevant_feedback = string_irrelevant_feedback.split(',')
                 IRRELEVANT_FEEDBACK.extend(irrelevant_feedback)
             RELEVANT_FEEDBACK.extend(relevant_feedback)
-            print("relevant feedback", RELEVANT_FEEDBACK)
-            print("irrelevant feedback", IRRELEVANT_FEEDBACK)
+            RELEVANT_FEEDBACK = list(set(RELEVANT_FEEDBACK))
+            IRRELEVANT_FEEDBACK = list(set(IRRELEVANT_FEEDBACK))
+            print("Relevant feedback", RELEVANT_FEEDBACK)
+            print("Irrelevant feedback", IRRELEVANT_FEEDBACK)
+            print("Gesture List", gesture_list)
         else:
             print("Exiting feedback loop")
             runAgain = False
             feedback = []
             break
+
 
 # Function to format result
 def formatResult(result):
@@ -108,6 +125,7 @@ def formatResult(result):
             resultString += str(res)
     return resultString
 
+
 root = tk.Tk()
 root.title("Task6-GUI")
 
@@ -117,22 +135,22 @@ canvas.pack()
 titleFrame = tk.Frame(root)
 titleFrame.place(relheight=0.2, relwidth=1)
 
-titleLabel = tk.Label(titleFrame, text='MWDB Phase 3 - Task 6', font=('Arial Bold',45))
+titleLabel = tk.Label(titleFrame, text='MWDB Phase 3 - Task 6', font=('Arial Bold', 45))
 titleLabel.place(relheight=1, relwidth=1)
 
 buttonFrame = tk.Frame(root)
 buttonFrame.place(relx=0.5, rely=0.2, relwidth=0.75, relheight=0.1, anchor='n')
 
-directoryButton = tk.Button(buttonFrame, text="Select Directory", command= lambda: getPath())
+directoryButton = tk.Button(buttonFrame, text="Select Directory", command=lambda: getPath())
 directoryButton.place(relx=0, relheight=1, relwidth=0.25)
 
-taskButton = tk.Button(buttonFrame, text="Select Task No.", command= lambda: getNum())
+taskButton = tk.Button(buttonFrame, text="Select Task No.", command=lambda: getNum())
 taskButton.place(relx=0.25, relheight=1, relwidth=0.25)
 
-gestureButton = tk.Button(buttonFrame, text="Select Query Gesture", command= lambda: getQuery())
+gestureButton = tk.Button(buttonFrame, text="Select Query Gesture", command=lambda: getQuery())
 gestureButton.place(relx=0.5, relheight=1, relwidth=0.25)
 
-startButton = tk.Button(buttonFrame, text="Generate Results", command= lambda: generateResults())
+startButton = tk.Button(buttonFrame, text="Generate Results", command=lambda: generateResults())
 startButton.place(relx=0.75, relheight=1, relwidth=0.25)
 
 resultFrame = tk.Frame(root, bg='#000000', bd=5)
--- Phase 3/task5.py
+++ Phase 3/task5.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import numpy as np
 
-def get_ppr2(qfile, k, m, c, gesture_list, relevant_gestures):
-    df = pd.read_csv("./task3_Edit_Dist_sim_mat.csv")
+
+def get_ppr2(k, c, gesture_list, relevant_gestures, irrelevant_gestures, gui=False):
+    relevant_gesture_percentage = 1
+    non_classified_gesture_percentage = 1 - relevant_gesture_percentage
+
+    non_classified_gestures = list(set(gesture_list) - set(relevant_gestures) - set(irrelevant_gestures))
+
+    # TODO: Make the file name uniform with task 3 input
+    df = pd.read_csv("./cosine_sim_matrix.csv")
     df = df.set_index('Unnamed: 0')
 
     A = df.copy(deep=True)
     # Getting the subset from the original similarity matrix
     A = A[gesture_list]
     A = A.loc[A.index.isin(gesture_list)]
-
 
     col_index_map = {}
     i = 0
@@ -18,20 +24,11 @@ def get_ppr2(qfile, k, m, c, gesture_list, relevant_gestures):
         col_index_map[i] = col
         i += 1
 
-    # for i in range(df.shape[0]):
-    #     temp = np.array(df.iloc[i, :])
-    #     topk = temp.argsort()[-k:][::-1]
-    #     for j in topk:
-    #         A.iloc[i, j] = df.iloc[i, j]
-
     # top k for new similarity graph
     for r in gesture_list:
         temp = np.array(df[gesture_list].loc[int(r)])
-        print('temp', temp)
         topk = temp.argsort()[-k:][::-1]
-        print('topk', topk)
         for j in topk:
-            print(r,j, temp[j])
             A.loc[int(r), col_index_map[j]] = temp[j]
 
     for i in range(A.shape[0]):
@@ -40,21 +37,22 @@ def get_ppr2(qfile, k, m, c, gesture_list, relevant_gestures):
 
     vecA = np.array(A)
     vq = np.zeros(A.shape[0])
-    if not relevant_gestures:
-        vq[A.columns.get_loc(qfile)] = 1
-    else:
-        length = len(relevant_gestures)
-        for file in relevant_gestures:
-            vq[A.columns.get_loc(file)] = 1 / length
+    length = len(relevant_gestures)
+    for file in relevant_gestures:
+        vq[A.columns.get_loc(file)] = (1 / length) * relevant_gesture_percentage
+    for file in non_classified_gestures:
+        vq[A.columns.get_loc(file)] = (1 / length) * non_classified_gesture_percentage
     uq = vq
     for i in range(1000):
         uq = (1 - c) * np.matmul(vecA, uq) + c * vq
 
-    print("\nQuery File: ", qfile, ".csv")
-    topm = uq.argsort()[-m:][::-1]
-    for it in topm:
+    topm = uq.argsort()[::-1]
+    output = ''
+    for i, it in enumerate(topm):
         print("File: ", A.columns[it], ", PPR: ", uq[it])
-
+        output += '{} - Gesture: {},\tSimilarity Score: {}\n'.format(i + 1, A.columns[it], np.round(uq[it], 3))
+    if gui:
+        return output
 
 
 def get_ppr(qfile, k, m, c, relevant=None):
@@ -108,14 +106,14 @@ if __name__ == '__main__':
     k = 30
     m = 10
     c = 0.85
-    gesture_list = ['1','11','2','31','4']
-    relevant_gestures = ['1','4']
-    get_ppr2(qfiles, k, m, c, gesture_list, relevant_gestures)
+    gesture_list = ['1', '11', '2', '31', '4']
+    relevant_gestures = ['1', '4']
+    irrelevant_gestures = ['11']
+    gesture_not_classified = list(set(gesture_list) - set(relevant_gestures) - set(irrelevant_gestures))
+    get_ppr2(qfiles, k, m, c, gesture_list, relevant_gestures, gesture_not_classified,gui=False)
 
     # print("Enter the relevant gestures (comma separated)")
     # relevant_gestures = input().split(",")
     # print("Enter the irrelavant gestures (comma separated)")
     # irrelevant_gestures = input().split(",")
     # get_ppr(qfiles, k, m, c, relevant)
-
-
