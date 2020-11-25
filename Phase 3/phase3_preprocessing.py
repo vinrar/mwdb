7fe525d8c589a4e796127422b2f5cde21a85322c
Shreesh Nayak <shreeshnayak@gmail.com>
2020-11-24 21:11:53 -0700

    Updated phase3 preprocessing

--- Phase 3/phase3_preprocessing.py
+++ Phase 3/phase3_preprocessing.py
@@ -4,14 +4,12 @@ import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 5:
-        print('Run python phase3_preprocessing.py <Directory> <Resolution> <Window Length> <Shift Length> <Vector '
-              'Model>')
+        print('Run python phase3_preprocessing.py <Directory> <Resolution> <Window Length> <Shift Length>)
         sys.exit(0)
     directory = sys.argv[1]
     r = sys.argv[2]
     w = sys.argv[3]
     s = sys.argv[4]
-    v_model = sys.argv[5]
 
     # print('Executing python phase3_task0a.py ' + directory + ' ' + r + ' ' + w + ' ' + s)
     os.system('python phase3_task0a.py ' + directory + ' ' + r + ' ' + w + ' ' + s)
